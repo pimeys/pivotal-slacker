0795f56a7ff0316a559493fb1d1bfe281c867030
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2013-02-05 14:08:45 +0100

    More solarized colors

--- lib/app_config.rb
+++ lib/app_config.rb
@@ -15,28 +15,28 @@ class AppConfig
     rescue Exception => e
       raise "No #{FILENAME} file found in current directory; please create one."
     end
-    
+
     AppConfig.new :config => config
   end
-  
+
   def initialize opts={:config => Hash.new}
     @config = opts[:config]
-    
+
     raise "Config must include \"project\" key" if not @config.has_key? "project"
     raise "Config must include \"api_key\" key" if not @config.has_key? "api_key"
     raise "Config must include \"user\" key" if not @config.has_key? "user"
   end
-  
+
   def project
     @config["project"]
   end
-  
+
   def api_key
     @config["api_key"]
   end
-  
+
   def user
     @config["user"]
   end
-  
+
 end
--- lib/formatter.rb
+++ lib/formatter.rb
@@ -6,26 +6,26 @@ class Formatter
   def self.story_id id
     id.to_s.color("#404040")
   end
-  
+
   def self.state state
     if state == "finished"
       # state = state.color('009900').bright #background('009900').color('ffffff')
-      " ✓ #{state} ".background('#4C9900').bright.underline
+      " ✓ #{state} ".color('#b58900').underline
     elsif state == "accepted"
-      " ✓ #{state} ".background('#0080ff').color(:black).underline
+      " ✓ #{state} ".background('#859900').underline
     else
       state = state.bright # .background(:green).underline
     end
   end
-  
+
   def self.requested_by requested_by
-    requested_by.color("#3399ff")
+    requested_by.color("#268bd2")
   end
-  
+
   def self.owner owner
     self.requested_by owner
   end
-  
+
   def self.story_name story_name, opts={:heading => false}
     if opts[:heading] == true
       story_name.bright.underline
@@ -33,32 +33,32 @@ class Formatter
       story_name
     end
   end
-  
+
   def self.time_ago time
-    (distance_of_time_in_words(Time.now, time) + " ago").color('#404040')
+    (distance_of_time_in_words(Time.now, time) + " ago").color('#fdf6e3')
   end
-  
+
   def self.note_author author
     self.requested_by(author)
   end
-  
+
   def self.note_text text
     text.bright
   end
-  
+
   def self.story_action comment, story_id, story_name
     story_name = Formatter.story_name(story_name, :heading => true)
     "#{comment} #{Formatter.story_id(story_id)}, #{story_name}"
   end
-  
+
   def self.story_type type
     type.color '#99004C'
   end
-  
+
   def self.attr_descriptor descriptor, attr
     "#{descriptor} → ".color('#333333') + attr
   end
-  
+
   def self.description description
     description
   end
